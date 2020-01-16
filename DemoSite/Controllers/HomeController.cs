0dec6aab046766983d18f335baa67a6653aba3d3
Matt Stewart <mstewart248@gmail.com>
2020-01-16 15:31:24 -0500

    Initial Commit

---
+++ DemoSite/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace DemoSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index(int? id)
+        {
+            var model = new Models.HomeModel();
+
+            model.RegulationsJSON = await FetchRegulationData(id);
+
+            return View(model);
+        }
+
+        public async Task<string> FetchRegulationData(int? id)
+        {
+            if(id == null)
+            {
+                id = 108;
+            }
+
+            var apiUrl = "https://rules.sos.ri.gov/regulations/api_get_rules_by_org_id_and_keyword/active/" + id.ToString();
+
+            using (var hc = new HttpClient { Timeout = TimeSpan.FromMilliseconds(1000 * 20) })
+            {
+                var response = await hc.GetAsync(apiUrl);
+                var res = await response.Content.ReadAsStringAsync();
+     
+                return res;
+            }
+        }
+
+
+    }
+}
---
+++ DemoSite/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoSite.Models
+{
+    public class HomeModel
+    {
+        public string RegulationsJSON { get; set; }
+
+    }
+}
